3ea65e351fedc45283692cd2fbf207aee8f6c5be
farinajn <farinajn@gmail.com>
2024-07-29 10:29:25 -0300

    Configuración front

--- ProyectoFinal/front/src/App.js
+++ ProyectoFinal/front/src/App.js
@@ -1,13 +1,22 @@
+import { BrowserRouter, Routes /*, Route*/ } from "react-router-dom";
+
 import "./App.css";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
+import HomePage from "./pages/HomePage";
+
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Nav></Nav>
+      <Header />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Routes path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
       <Footer></Footer>
     </div>
   );
---
+++ ProyectoFinal/front/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+const HomePage = (props) => {
+  return (
+    <main className="holder">
+      <div className="homeimg">
+        <img src="/front/public/img/home/logo ACCMA.jpg" alt="logoACCMA" />
+      </div>
+    </main>
+  );
+};
+
+export default HomePage;
