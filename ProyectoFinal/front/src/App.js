757aa6b1518eb460a93ef1190199c277defd3642
farinajn <farinajn@gmail.com>
2024-07-29 00:08:19 -0300

    Configuración inicial front

---
+++ ProyectoFinal/front/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Nav></Nav>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
