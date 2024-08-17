60c186eefe57540d43b9ee731c1c23caaf16cd36
farinajn <farinajn@gmail.com>
2024-08-16 23:48:01 -0300

    Se corrigen las variables de entorno y quedan efectivamente conectada la base de datos

--- ProyectoFinal/back/models/bd.js
+++ ProyectoFinal/back/models/bd.js
@@ -3,10 +3,10 @@ var util = require("util");
 
 var pool = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "proyecto_final",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB_NAME,
 });
 
 pool.query = util.promisify(pool.query);
